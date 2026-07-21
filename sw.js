3b5747d4971a71e9c0e2060c07bba621065e4171
timverschot <timothyverschoren@gmail.com>
2026-07-22 01:42:50 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  * Versienummer ophogen forceert een schone installatie bij je volgende bezoek.
  */
 
-const VERSION = 'v8';
+const VERSION = 'v9';
 const SHELL_CACHE = `mediacollectie-shell-${VERSION}`;
 const IMAGE_CACHE = `mediacollectie-images-${VERSION}`;
 const MAX_IMAGES = 600;
